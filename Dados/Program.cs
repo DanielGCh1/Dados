367dd888981e0528f28d0abbf60b31870da3bec4
DanielGCh1 <dgchaarturo@gmail.com>
2019-12-05 19:59:18 -0600

    Cumpliendo el punto 1

---
+++ Dados/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dados
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string mensaje = "Nueva visualización";
+            int enter = 13;
+            int consola = 0;
+            do {
+                Console.WriteLine(mensaje);
+                Console.ReadKey();
+                consola = Console.Read();
+                Console.Read();
+            } while (consola == enter);
+        }
+    }
+}
